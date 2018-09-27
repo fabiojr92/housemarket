8159e80cf1a07d647f784a52d30debd23f920bdb
Fábio Junior <jr_fjc@hotmail.com>
2018-09-26 21:53:14 -0400

    sprint 4
    sprint 4

--- src/main/java/housemarket/rodolforoca/com/DAO/EnderecoRepository.java
+++ src/main/java/housemarket/rodolforoca/com/DAO/EnderecoRepository.java
@@ -1,9 +1,11 @@
 package housemarket.rodolforoca.com.DAO;
 
-import housemarket.rodolforoca.com.Model.Endereco;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+import housemarket.rodolforoca.com.Model.Anuncio;
+import housemarket.rodolforoca.com.Model.Endereco;
 
 public interface EnderecoRepository extends CrudRepository<Endereco, Long> {
 
